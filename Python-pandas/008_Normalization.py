d4ac44fba8bf879314dc8e326e88077b4f7eaa7d
tasin-science <science1711362@gmail.com>
2024-12-01 17:37:35 +0000

    008_Normalization.py

---
+++ Python-pandas/008_Normalization.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv("Datasheet/Dataset_Lab_Test.csv")
+print(df, "\n\n")
+
+#Normalize Product A
+df['Normalized_Product_A'] = (df['Product_A'] - df['Product_A'].min()) / (df['Product_A'].max() - df['Product_A'].min())
