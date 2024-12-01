440179a8c020882f75e7f06a1d1912d64a1d930d
tasin-science <science1711362@gmail.com>
2024-12-01 18:04:14 +0000

    a

--- Python-numpy/001_Numpy_intro.py
+++ Python-numpy/001_Numpy_intro.py
@@ -7,10 +7,10 @@ cvalues = [20.1, 20.8, 21.9, 22.5, 22.7, 22.3, 21.8, 21.2, 20.9,
 20.1]
 # converting this list into one-dimensional Numpy array
 C = np.array(cvalues)
-print(cvalues)
-print(type(cvalues))
-print(C)
-print(type(C))
+print("C value list: ", cvalues)
+print("The type of c value list is: ", type(cvalues), "\n")
+print("Celcius nympy array: ", C)
+print( "The type of Celcius nympy array is: ", type(C), "\n")
 
 F = C * 9/5 + 32
-print(F)
+print("Farenheit nympy array: ", F)
