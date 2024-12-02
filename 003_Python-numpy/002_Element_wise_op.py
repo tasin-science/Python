ab5f5c3cca870f9d2a18b112b8204fd3149d1463
tasin-science <science1711362@gmail.com>
2024-12-02 16:26:28 +0000

    a

--- 003_Python-numpy/002_Element_wise_op.py
+++ 003_Python-numpy/002_Element_wise_op.py
@@ -4,7 +4,7 @@ import numpy as np
 
 A = np.array([[1,2,3],[4,5,6]])
 print(A)
-print(A.shape)
+print(A.shape) #shape shows (number of rows, number of columns)
 B = np.array([[7,8,9],[10,11,12]])
 print(B)
 print(B.shape)
