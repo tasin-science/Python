c07e1504b97868d67a37be18e0a79515d7795447
tasin-science <science1711362@gmail.com>
2024-12-06 14:33:00 +0000

    a

--- 003_Python-numpy/004_Numpy_simple_math.py
+++ 003_Python-numpy/004_Numpy_simple_math.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 a = np.array([[1,2],[3,4]], dtype = np.float64)
-b = np.array([[5,6],[7,8]], dtype = np.float64])
+b = np.array([[5,6],[7,8]], dtype = np.float64)
 
 ### using  arithmetic symbols (+ - * /)
 print(a + b, "\n")
