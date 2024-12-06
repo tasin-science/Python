7c76472defc6d9e06b8528c4538d137048cef9a0
tasin-science <science1711362@gmail.com>
2024-12-06 14:32:28 +0000

    a

---
+++ 003_Python-numpy/004_Numpy_simple_math.py
@@ -0,0 +1,12 @@
+#### Numpy Simple Math ####
+
+import numpy as np
+
+a = np.array([[1,2],[3,4]], dtype = np.float64)
+b = np.array([[5,6],[7,8]], dtype = np.float64])
+
+### using  arithmetic symbols (+ - * /)
+print(a + b, "\n")
+print(a - b, "\n")
+print(a * b, "\n")
+print(a / b, "\n")
