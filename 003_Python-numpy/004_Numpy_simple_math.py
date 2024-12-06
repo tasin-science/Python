5c30a4cd9a3e716c23340c30fb12e7c251999b50
tasin-science <science1711362@gmail.com>
2024-12-06 14:40:08 +0000

    a

--- 003_Python-numpy/004_Numpy_simple_math.py
+++ 003_Python-numpy/004_Numpy_simple_math.py
@@ -4,16 +4,24 @@ import numpy as np
 
 a = np.array([[1,2],[3,4]], dtype = np.float64)
 b = np.array([[5,6],[7,8]], dtype = np.float64)
+c = np.array([[4,9],[16,25]])
 
 ### using  arithmetic symbols (+ - * /)
+print("Using arithmetic symbols (+ - * /)")
 print(a + b, "\n")
 print(a - b, "\n")
 print(a * b, "\n")
 print(a / b, "\n")
-
+print("\n")
 
 #### using arithmetic function
+print("Using arithmetic function")
 print(np.add(a, b), "\n")
 print(np.subtract(a, b), "\n")
 print(np.multiply(a, b), "\n")
 print(np.divide(a, b), "\n")
+print("\n")
+
+### square root function
+print("square root of c")
+print(np.sqrt(c), "\n")
