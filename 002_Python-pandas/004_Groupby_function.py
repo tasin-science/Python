8a9cbcada069a04b6d1d6d781df6f03990375407
tasin-science <science1711362@gmail.com>
2024-12-14 16:47:20 +0000

    a

--- 002_Python-pandas/004_Groupby_function.py
+++ 002_Python-pandas/004_Groupby_function.py
@@ -2,14 +2,16 @@
 
 import pandas as pd
 df = pd.read_csv("Datasheet/Dataset_Lab_Test.csv")
-print(df, "\n\n")
+print(df, "\n\n\n")
 
 ### Group by year and calculate the average of 'Product A'
 print("Calculate the averages of Product A according years")
-print(df.groupby('Year')['Product_A'].mean(), "\n\n")
+print(df.groupby('Year')['Product_A'].mean(), "\n\n\n")
 
 ### Group by year and calculate the average of 'Product A', 'Product B' and 'Product C'
 print("Calculate the averages of Product A, 'Product B' and 'Product C' according years")
-print(df.groupby('Year')[['Product_A', 'Product_B', 'Product_C']].mean(), "\n\n")
+print(df.groupby('Year')[['Product_A', 'Product_B', 'Product_C']].mean(), "\n\n\n")
 
+### Group by nothing
+print(df.groupby('\0')[['Product_A', 'Product_B', 'Product_C']].mean(), "\n\n\n")
 
