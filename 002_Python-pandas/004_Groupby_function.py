1fe665a744edc534ca9b8e009d6fc9bb4b0fecba
tasin-science <science1711362@gmail.com>
2024-12-14 17:00:40 +0000

    a

--- 002_Python-pandas/004_Groupby_function.py
+++ 002_Python-pandas/004_Groupby_function.py
@@ -11,10 +11,3 @@ print(df.groupby('Year')['Product_A'].mean(), "\n\n\n")
 ### Group by year and calculate the average of 'Product A', 'Product B' and 'Product C'
 print("Calculate the averages of Product A, 'Product B' and 'Product C' according years")
 print(df.groupby('Year')[['Product_A', 'Product_B', 'Product_C']].mean(), "\n\n\n")
-
-dictionary = {
-    'Product_A' : df['Product_A'].mean(),
-    'Product_B' : df['Product_B'].mean(),
-    'Product_C' : df['Product_C'].mean()
-}
-print(dictionary)
