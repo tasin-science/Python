7ac7f67550b319d97a849b5f66d7b013265a7472
tasin-science <science1711362@gmail.com>
2024-12-17 13:05:11 +0000

    a

--- 002_Python-pandas/001_Pandas_intro.py
+++ 002_Python-pandas/001_Pandas_intro.py
@@ -6,7 +6,7 @@ import pandas as pd
 data = pd.Series([1,2,3,4,5])
 print(data)
 
-print("\n\n")
+print("\n\n\n")
 
 ### Creating a dataframe from dictionary
 source = {
@@ -18,12 +18,12 @@ df = pd.DataFrame(source)
 print(df)
 print(type(df))
 
-print("\n\n")
+print("\n\n\n")
 
 
 ### Reading a Spreadsheet file
 df = pd.read_csv("Datasheet/Dataset_Lab_Test.csv")
-print(df, "\n")
+print(df, "\n\n\n")
 
 
 
@@ -32,19 +32,23 @@ print(df, "\n")
 rows, columns = df.shape
 print("Rows are: ", rows, "\nColumns are: ", columns)
 
-print("\n\n")
+print("\n\n\n")
 
 
 ### head() and tail() method
-print(df.head(), "\n\n") ## head() method
-print(df.head(10), "\n\n") ## first 10 rows
-print(df.tail(), "\n\n") ## tail() method
-print(df.tail(10), "\n\n") ## last 10 rows
+print(df.head(), "\n\n\n") ## head() method
+print(df.head(10), "\n\n\n") ## first 10 rows
+print(df.tail(), "\n\n\n") ## tail() method
+print(df.tail(10), "\n\n\n") ## last 10 rows
 
 
 ### creating new column
 df['New_Col'] = 0
-print(df)
+print(df, "\n\n\n")
+#<>
+## creating new column with arithmetic expression
+df['A+B+C'] = df['Product_A'] + df['Product_B'] + df['Product_C']
+print(df, "\n\n\n")
 
 
 
