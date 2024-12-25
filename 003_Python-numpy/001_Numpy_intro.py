023bba77c579b8343f588135723d19bcc1b9124c
tasin-science <science1711362@gmail.com>
2024-12-25 07:36:04 +0000

    a

--- 003_Python-numpy/001_Numpy_intro.py
+++ 003_Python-numpy/001_Numpy_intro.py
@@ -8,7 +8,7 @@ cvalues = [20.1, 20.8, 21.9, 22.5, 22.7, 22.3, 21.8, 21.2, 20.9, 20.1]
 C = np.array(cvalues)
 print("C value list: ", cvalues)
 print("The type of c value list is: ", type(cvalues), "\n")
-print("Celcius nympy array: ", C)
+print("Celcius numpy array: ", C)
 print( "The type of Celcius nympy array is: ", type(C), "\n")
 
 F = C * 9/5 + 32
