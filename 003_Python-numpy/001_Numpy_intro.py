a15b9a9f2a71eb5a15b90030e5f572a748678144
tasin-science <science1711362@gmail.com>
2024-12-26 10:49:33 +0000

    a

--- 003_Python-numpy/001_Numpy_intro.py
+++ 003_Python-numpy/001_Numpy_intro.py
@@ -3,13 +3,20 @@
 import numpy as np
 
 # create a Python list of temperature in degree celcius
+print("This is the numpy program of temperature converter. here C list contains celcius temperatures")
 cvalues = [20.1, 20.8, 21.9, 22.5, 22.7, 22.3, 21.8, 21.2, 20.9, 20.1]
 # converting this list into one-dimensional Numpy array
 C = np.array(cvalues)
 print("C value list: ", cvalues)
 print("The type of c value list is: ", type(cvalues), "\n")
 print("Celcius numpy array: ", C)
-print( "The type of Celcius nympy array is: ", type(C), "\n")
+print( "The type of Celcius numpy array is: ", type(C), "\n")
 
 F = C * 9/5 + 32
-print("Farenheit nympy array: ", F)
+print("Farenheit numpy array: ", F)
+print("\n\n\n")
+
+### Converting numpy array as normal list4
+print("We want to make farenheit numpy array ", F, " as farenheit temperature list")
+Far_list = list(F)
+print("Farenheit temperature list is: ", Far_list)
