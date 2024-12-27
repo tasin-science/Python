63798fc523dd5f640d1cd5ca59839bd94c3d87d0
tasin-science <science1711362@gmail.com>
2024-12-27 10:57:29 +0000

    a

--- 003_Python-numpy/004_Numpy_arithmetic_operation.py
+++ 003_Python-numpy/004_Numpy_arithmetic_operation.py
@@ -9,21 +9,21 @@ c = np.array([[4,9],[16,25]])
 print("Matrix a is: \n", a, "\n")
 print("Matrix b is: \n", b, "\n")
 print("Matrix c is: \n", c, "\n")
-
+print("\n\n")
 
 
 ### using  arithmetic symbols (+ - * /)
-print("Using arithmetic symbols (+ - * /)")
+print("Using arithmetic symbols (+ - * /) \n")
 print("a + b: \n", a + b, "\n") # matrix a + matrix b 
 print("a - b: \n", a - b, "\n") # matrix a - matrix b
 print("a * b: \n", a * b, "\n") # matrix a * matrix b
 print("a / b: \n", a / b, "\n") # matrix a / matrix b
-print("\n")
+print("\n\n")
 
 
 
 #### using arithmetic function
-print("Using arithmetic function")
+print("Using arithmetic function \n")
 print("add(a, b) = a + b \n", np.add(a, b), "\n")  
 print("subtract(a, b) = a - b \n", np.subtract(a, b), "\n")  
 print("multiply(a, b) = a * b \n", np.multiply(a, b), "\n")  
