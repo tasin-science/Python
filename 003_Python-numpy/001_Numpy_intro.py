1aec882be40af84a1abf265d0f0c5b5797f431b9
tasin-science <science1711362@gmail.com>
2024-12-27 11:23:17 +0000

    a

--- 003_Python-numpy/001_Numpy_intro.py
+++ 003_Python-numpy/001_Numpy_intro.py
@@ -3,6 +3,7 @@
 import numpy as np
 
 # create a Python list of temperature in degree celcius
+print("//Basic of numpy array//")
 print("This is the numpy program of temperature converter. here C list contains celcius temperatures")
 cvalues = [20.1, 20.8, 21.9, 22.5, 22.7, 22.3, 21.8, 21.2, 20.9, 20.1]
 # converting this list into one-dimensional Numpy array
@@ -26,30 +27,33 @@ print("\n\n\n")
 
 
 ### Matrix/2D list to matrix numpy
+print("//Two dimension//")
 m_list = [[1,2],[3,4]]
-print("Our two dimension list is: ", m_list)
+print("Two dimension list is: ", m_list)
 M = np.array(m_list)
 print("The numpy matrix of that two dimension list is:\n", M)
 print("\n\n\n")
 
 
-### types of numpy array
+### data types of numpy array
 print("//Types of numpy array//")
 print("\n")
 A1 = np.array([1, 2, 3, 4])
 print("A1 numpy array is: ", A1)
-print("type of A1 is: ", type(A1))
+print("type of A1 is: ", A1.dtype)
 print("\n")
 #<>
 A2 = np.array([1, 2, 3, 4], dtype = np.float64)
 print("A2 numpy array is: ", A2)
-print("type of A2 is: ", type(A2))
+print("type of A2 is: ", A2.dtype)
 print("\n")
 #<>
 A3 = np.array([1, 2, 3, 4], dtype = np.complex128)
 print("A3 numpy array is: ", A3)
-print("type of A3 is: ", A3.dtype) # we can also write A3.dtype, which is as same as type(A3)
+print("type of A3 is: ", A3.dtype)
+print("\n")
 #<>
 A4 = np.array([1, 2, 3, 4], dtype = np.bool_)
 print("A4 numpy array is: ", A4)
 print("type of A4 is: ", A4.dtype)
+print("\n\n\n")
