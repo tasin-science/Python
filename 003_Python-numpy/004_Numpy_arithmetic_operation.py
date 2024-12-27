25dbd59ae115cafe72419a871b4a6c2d892da62a
tasin-science <science1711362@gmail.com>
2024-12-27 10:55:32 +0000

    a

--- 003_Python-numpy/004_Numpy_arithmetic_operation.py
+++ 003_Python-numpy/004_Numpy_arithmetic_operation.py
@@ -6,22 +6,31 @@ a = np.array([[1,2],[3,4]], dtype = np.float64)
 b = np.array([[5,6],[7,8]], dtype = np.float64)
 c = np.array([[4,9],[16,25]])
 
+print("Matrix a is: \n", a, "\n")
+print("Matrix b is: \n", b, "\n")
+print("Matrix c is: \n", c, "\n")
+
+
+
 ### using  arithmetic symbols (+ - * /)
 print("Using arithmetic symbols (+ - * /)")
-print(a + b, "\n")
-print(a - b, "\n")
-print(a * b, "\n")
-print(a / b, "\n")
+print("a + b: \n", a + b, "\n") # matrix a + matrix b 
+print("a - b: \n", a - b, "\n") # matrix a - matrix b
+print("a * b: \n", a * b, "\n") # matrix a * matrix b
+print("a / b: \n", a / b, "\n") # matrix a / matrix b
 print("\n")
+
+
 
 #### using arithmetic function
 print("Using arithmetic function")
-print(np.add(a, b), "\n")
-print(np.subtract(a, b), "\n")
-print(np.multiply(a, b), "\n")
-print(np.divide(a, b), "\n")
+print("add(a, b) = a + b \n", np.add(a, b), "\n")  
+print("subtract(a, b) = a - b \n", np.subtract(a, b), "\n")  
+print("multiply(a, b) = a * b \n", np.multiply(a, b), "\n")  
+print("divide(a, b) = a / b \n", np.divide(a, b), "\n") 
 print("\n")
 
+
 ### square root function
-print("square root of c")
+print("square root of c: \n")
 print(np.sqrt(c), "\n")
