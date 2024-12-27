a82dfcf71cd700e47797dffd3394ccecc1f85a56
tasin-science <science1711362@gmail.com>
2024-12-27 14:28:59 +0000

    a

--- 004_Python-matplotlib/001_Matplotlib_intro.py
+++ 004_Python-matplotlib/001_Matplotlib_intro.py
@@ -1,4 +1,10 @@
 #### Matplotlib Intro ####
 
 import matplotlib.pyplot as plt
+import numpy as np
 
+# xpts and ypts for co-ordinates
+xpts = np.array([0, 10])
+ypts = np.array([0, 125])
+plt.plot(xpts, ypts)
+plt.show()
